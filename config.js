c8b9f75c618921a17030e61f8309692b86fb1852
Parker Ly <Parker_Ly@mckinsey.com>
2022-06-04 23:14:24 +0700

    add func to get exchange rates

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+  cryptocompare_key: '1f504047629a63b9c9a8d8795ad5744f28ec48cbe31ac07d366faa9249249642'
+};
+module.exports = config;
--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const fs = require('fs');
 const _ = require('lodash');
 const GenerateCSV = require('./generateCSV');
 const moment = require('moment');
-
-const data = [
-  ['0A', '0B', '0C'],
-  ['1A', '1B', '1C'],
-  ['2A', '2B', '2C']
-];
+const config = require('./config');
+const axios = require('axios').default;
+const Promise = require('bluebird');
 
 class PortfolioManager {
   constructor (args) {
@@ -45,7 +42,10 @@ class PortfolioManager {
       let { date } = args;
       date = date === true ? undefined : date;
 
-      console.log(this.getValue({ date, token }, data));
+      // console.log(this.getValue({ date, token }, data));
+      const tableData = await this.getValue({ date, token }, data);
+      tableData.unshift(['TOKEN', 'AMOUNT', 'VALUATION (USD)', 'DATE']);
+      console.log(table(tableData));
     }
   }
 
@@ -57,7 +57,7 @@ class PortfolioManager {
 
   loadCSV (filepath) {
     const results = [];
-    const promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       fs.createReadStream(filepath)
         .pipe(csv())
         .on('data', (data) => {
@@ -68,32 +68,46 @@ class PortfolioManager {
           });
         })
         .on('end', () => {
-          this.debug(results);
           resolve(results);
         })
-        .on('error', (e) => { reject(e); });
+        .on('error', (e) => {
+          reject(e);
+        });
     });
-    return promise;
   }
 
-  getValue ({ date, token }, data) {
-    const result = {};
-    _.each(data, d => {
-      if (result[d.token] === undefined) {
-        result[d.token] = 0;
+  getExchangeRates (symbol, timestamp) {
+    return axios.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=${symbol}&tsyms=USD&ts=${timestamp}`, {
+      headers: {
+        authorization: 'Apikey ' + config.cryptocompare_key
+      }
+    })
+      .then(({ data }) => {
+        return data;
+      })
+      .catch((error) => {
+        this.debug(error);
+      });
+  }
+
+  async getValue ({ date, token }, data) {
+    let result = {};
+    _.each(data, record => {
+      if (result[record.token] === undefined) {
+        result[record.token] = 0;
       }
 
       const calculateCurrentPosition = () => {
-        if (d.transaction_type === 'WITHDRAWAL') {
-          result[d.token] -= d.amount;
+        if (record.transaction_type === 'WITHDRAWAL') {
+          result[record.token] -= record.amount;
         } else {
-          result[d.token] += d.amount;
+          result[record.token] += record.amount;
         }
       };
 
       if (date) {
         const mParamDate = moment(date).endOf('d');
-        const mDate = moment(data.timestamp);
+        const mDate = moment(record.timestamp);
         if (mDate.isSameOrBefore(mParamDate)) {
           calculateCurrentPosition();
         }
@@ -103,9 +117,25 @@ class PortfolioManager {
     });
 
     if (token) {
-      return _.pick(result, token);
+      result = _.pick(result, token);
     }
-    return result;
+
+    const promiseArr = [];
+    const timestamp = date ? moment(date).unix() : moment().unix();
+    _.each(result, (value, key) => {
+      promiseArr.push(this.getExchangeRates(key, timestamp));
+    });
+
+    return Promise.all(promiseArr).then((exchangeRateResult) => {
+      const exchangeRates = _.merge({}, ...exchangeRateResult);
+      console.log(exchangeRates);
+      const convertedResult = [];
+      _.each(result, (value, key) => {
+        const convertedValue = value * exchangeRates[key].USD;
+        convertedResult.push([key, value, convertedValue, moment.unix(timestamp).format('YYYY-MM-DD')]);
+      });
+      return convertedResult;
+    });
   }
 }
 
