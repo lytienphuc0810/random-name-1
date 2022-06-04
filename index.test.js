58fe91ace34b9fc71142b921a02165577af90bf8
Parker Ly <Parker_Ly@mckinsey.com>
2022-06-05 05:29:16 +0700

    jest test for other methods

--- index.test.js
+++ index.test.js
@@ -1,5 +1,8 @@
 const PortfolioManager = require('./index');
 const moment = require('moment');
+const axios = require('axios');
+const GenerateCSV = require('./generateCSV');
+jest.mock('./generateCSV');
 
 describe('PortfolioManager', () => {
   const data = [
@@ -15,17 +18,18 @@ describe('PortfolioManager', () => {
   let getExchangeRatesMock;
   let pManager;
   const mockDateString = moment().format('YYYY-MM-DD');
-  beforeEach(() => {
-    const args = {
-      f: './data.csv'
-    };
-    pManager = new PortfolioManager(args);
-  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   describe('getValue', () => {
+    beforeEach(() => {
+      const args = {
+        f: './data.csv'
+      };
+      pManager = new PortfolioManager(args);
+    });
     it('should run correctly with no additional params', async () => {
       getExchangeRatesMock = jest.spyOn(pManager, 'getExchangeRates')
         .mockResolvedValueOnce({ SHIB: { USD: 0.2 } })
@@ -84,6 +88,96 @@ describe('PortfolioManager', () => {
 
       expect(result[0]).toEqual(['DOGE', 0.7994686780411233 - 0.43485920355747393 * 3,
         (0.7994686780411233 - 0.43485920355747393 * 3) * 0.5, '2020-01-04']);
+    });
+  });
+
+  describe('run', () => {
+    describe('f argument', () => {
+      it('should run correctly in case of no addition filepath', () => {
+        pManager = new PortfolioManager({ f: true });
+        const loadCSVMock = jest.spyOn(pManager, 'loadCSV')
+          .mockResolvedValueOnce([]);
+        pManager.run({ f: true });
+        expect(loadCSVMock).toBeCalledWith('./data.csv');
+      });
+      it('should run correctly in case of missing f', () => {
+        pManager = new PortfolioManager({ });
+        const loadCSVMock = jest.spyOn(pManager, 'loadCSV')
+          .mockResolvedValueOnce([]);
+        pManager.run({ });
+        expect(loadCSVMock).not.toBeCalled();
+      });
+      it('should run correctly in case filepath exists', () => {
+        pManager = new PortfolioManager({ f: 'mockdata.csv' });
+        const loadCSVMock = jest.spyOn(pManager, 'loadCSV')
+          .mockResolvedValueOnce([]);
+        pManager.run({ f: 'mockdata.csv' });
+        expect(loadCSVMock).toBeCalledWith('mockdata.csv');
+      });
+      it('should handle exceptions', () => {
+        const NotFoundErr = new Error('not found');
+        pManager = new PortfolioManager({ f: 'mockdata.csv' });
+        const loadCSVMock = jest.spyOn(pManager, 'loadCSV').mockImplementation(() => {
+          throw NotFoundErr;
+        });
+        const debugMock = jest.spyOn(pManager, 'debug');
+
+        pManager.run({ f: 'mockdata.csv' });
+        expect(loadCSVMock).toBeCalledWith('mockdata.csv');
+        expect(debugMock).toBeCalled();
+      });
+    });
+    describe('g argument', () => {
+      it('should run correctly in case filepath exists', () => {
+        pManager = new PortfolioManager({ g: 'data.csv' });
+        jest.spyOn(pManager, 'loadCSV').mockResolvedValueOnce([]);
+        pManager.run({ g: 'data.csv' });
+        expect(GenerateCSV).toBeCalled();
+      });
+      it('should handle exceptions', async () => {
+        pManager = new PortfolioManager({ g: true });
+
+        await expect(pManager.run({ g: true }))
+          .rejects
+          .toThrowError('filepath missing');
+      });
+    });
+    it('should token & date argument correctly', () => {});
+  });
+  describe('debug', () => {
+    it('should log when enabled', () => {
+      const consoleMock = jest.spyOn(console, 'log').mockImplementation(() => {});
+      pManager = new PortfolioManager({ v: true });
+      pManager.debug('');
+      expect(consoleMock).toBeCalledTimes(1);
+    });
+    it('should not log when disabled', () => {
+      const consoleMock = jest.spyOn(console, 'log').mockImplementation(() => {});
+      pManager = new PortfolioManager({ v: false });
+      pManager.debug('');
+      expect(consoleMock).not.toBeCalled();
+    });
+  });
+  describe('loadCSV', () => {
+
+  });
+  describe('getExchangeRates', () => {
+    it('show call the correct API with correct query string', async () => {
+      const axiosMock = jest.spyOn(axios, 'get').mockResolvedValueOnce({});
+      pManager = new PortfolioManager({});
+      await pManager.getExchangeRates('DOGE', 1654333919);
+      expect(axiosMock).toBeCalledWith('https://min-api.cryptocompare.com/data/pricehistorical?fsym=DOGE&tsyms=USD&ts=1654333919', expect.anything());
+    });
+
+    it('show log error in case of error', async () => {
+      const error = new Error('mock');
+      const axiosMock = jest.spyOn(axios, 'get').mockRejectedValue(error);
+      pManager = new PortfolioManager({});
+      const debugMock = jest.spyOn(pManager, 'debug');
+
+      await pManager.getExchangeRates('DOGE', 1654333919);
+      expect(axiosMock).toBeCalledWith('https://min-api.cryptocompare.com/data/pricehistorical?fsym=DOGE&tsyms=USD&ts=1654333919', expect.anything());
+      expect(debugMock).toBeCalledWith(error);
     });
   });
 });
