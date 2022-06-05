ab287fe89770172c22c3fa9de138cc94e0af58fc
Parker Ly <Parker_Ly@mckinsey.com>
2022-06-05 16:05:07 +0700

    command options

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,43 @@
 #! /usr/bin/env node
 
 const yargs = require('yargs');
-const { hideBin } = require('yargs/helpers');
-
-const { argv } = yargs(hideBin(process.argv));
-
 const PortfolioManager = require('../index');
 
-const usage = '\nUsage: tran <lang_name> sentence to be translated';
-
-yargs
+const usage = '\nUsage: tokenquote -f <csv_filepath>';
+const options = yargs
   .usage(usage)
   .option('l', {
     alias: 'languages',
     describe: 'List all supported languages.',
     type: 'boolean',
     demandOption: false
-  }).help(true)
+  })
+  .option('v', {
+    describe: 'Display logs',
+    type: 'boolean',
+    demandOption: false
+  })
+  .option('g', {
+    describe: 'Generate the test CSV & save it to the passed filepath',
+    type: 'string',
+    demandOption: false
+  })
+  .option('f', {
+    describe: 'Pass the csv file to be parsed',
+    type: 'string',
+    demandOption: true
+  })
+  .option('token', {
+    describe: 'Pass token to show only transactions associate with this token',
+    type: 'string',
+    demandOption: false
+  })
+  .option('date', {
+    describe: 'Pass date to show only transactions before or equal this date',
+    type: 'string',
+    demandOption: false
+  })
+  .help(true)
   .argv;
 
-new PortfolioManager(argv).run(argv);
+new PortfolioManager(yargs.argv).run(yargs.argv);
